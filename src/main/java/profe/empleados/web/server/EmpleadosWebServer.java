3bc080de472a2de3fc0ca01b42e9475b1ccc197b
made <kike@mazarredos.com>
2018-11-16 14:58:14 +0100

    Configurado sin funcionar
    El primer intento es utilizar un RestTemplate request scoped con
    proxyScoped. No me despliega porque me da una excepción de No
    thread-bound request found... Añadí un RequestContextListener al
    contexto según stackoverflow, seguía dando y resulta que si le quito el
    @LoadBalanced sí despliega (pero claro, ya no es loadbalanced XD)

--- src/main/java/profe/empleados/web/server/EmpleadosWebServer.java
+++ src/main/java/profe/empleados/web/server/EmpleadosWebServer.java
@@ -2,17 +2,26 @@ package profe.empleados.web.server;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.cloud.openfeign.FeignClientsConfiguration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Import;
+import org.springframework.context.annotation.Scope;
+import org.springframework.context.annotation.ScopedProxyMode;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.web.context.request.RequestContextListener;
 
 import profe.empleados.web.controllers.EmpleadosWebController;
 import profe.empleados.web.controllers.HomeController;
 import profe.empleados.web.security.EmpleadosWebSecurityConfig;
 import profe.empleados.web.service.EmpleadosWebService;
-import profe.empleados.web.service.EmpleadosWebServiceRibbon;
+import profe.empleados.web.service.EmpleadosWebServiceRibbonLBAuth;
+import profe.empleados.web.service.exceptions.RestTemplateErrorHandler;
 import profe.empleados.web.validator.EmpleadoValidator;
 
 @SpringBootApplication
@@ -46,13 +55,29 @@ public class EmpleadosWebServer {
 
 	@Bean
 	public EmpleadosWebService webService() {
-		return new EmpleadosWebServiceRibbon();
+		return new EmpleadosWebServiceRibbonLBAuth();
 	}
 
 	@Bean
 	public EmpleadoValidator empleadoValidator() {
 		return new EmpleadoValidator();
 	}
+	
+	@Bean
+//	@LoadBalanced
+	@Scope(value="request", proxyMode=ScopedProxyMode.TARGET_CLASS)
+	public RestTemplate restTemplate(RestTemplateBuilder restTemplateBuilder) {
+		Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+		return restTemplateBuilder
+				.basicAuthorization(auth.getName(), (String) auth.getCredentials())
+				.errorHandler(new RestTemplateErrorHandler())
+				.build();
+
+	}
+	
+	@Bean public RequestContextListener requestContextListener(){
+	    return new RequestContextListener();
+	} 
 	
 	@Bean
 	public EmpleadosWebSecurityConfig empleadosSecurityConfig() {
---
+++ src/main/java/profe/empleados/web/service/EmpleadosWebServiceRibbonLBAuth.java
@@ -0,0 +1,114 @@
+package profe.empleados.web.service;
+
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.logging.Logger;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.cloud.client.loadbalancer.LoadBalancerClient;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.client.HttpClientErrorException;
+import org.springframework.web.client.RestTemplate;
+
+import profe.empleados.web.model.Empleado;
+import profe.empleados.web.service.exceptions.EmpleadosWebException;
+import profe.empleados.web.service.exceptions.EmpleadosWebNotAuthorizedException;
+import profe.empleados.web.service.exceptions.EmpleadosWebResourceDuplicatedException;
+import profe.empleados.web.service.exceptions.EmpleadosWebResourceNotFoundException;
+import profe.empleados.web.service.exceptions.RestTemplateErrorHandler;
+
+/**
+ * Hide the access to the microservice inside this local service.
+ * 
+ * Implementación que usa Ribbon
+ * 
+ * @author Paul Chapman - Versión de Enrique Pedraza
+ */
+public class EmpleadosWebServiceRibbonLBAuth implements EmpleadosWebService {
+
+	@Autowired
+//	@LoadBalanced
+	private RestTemplate restTemplate;
+	
+	@Value("${app.serviceAlias}")
+	protected String serviceAlias;
+
+	protected Logger logger = Logger.getLogger(EmpleadosWebServiceRibbonLBAuth.class
+			.getName());
+	
+	private String getBaseUrl() {
+		return "http://" + this.serviceAlias;
+	}
+	
+	/* (non-Javadoc)
+	 * @see profe.empleados.web.service.EmpleadosWebService#getEmpleado(java.lang.String)
+	 */
+	@Override
+	public Empleado getEmpleado(String cif) throws EmpleadosWebResourceNotFoundException {
+		return restTemplate.getForObject(this.getBaseUrl() + "/empleados/{cif}",
+				Empleado.class, cif);
+	}
+
+	/* (non-Javadoc)
+	 * @see profe.empleados.web.service.EmpleadosWebService#eliminaEmpleado(java.lang.String)
+	 */
+	@Override
+	public void eliminaEmpleado(String cif) 
+			throws EmpleadosWebResourceNotFoundException, EmpleadosWebNotAuthorizedException {
+		try {
+			restTemplate.delete(this.getBaseUrl() + "/empleados/{cif}", cif);
+		} catch (HttpClientErrorException e) {
+			logger.info("error al eliminar el empleado");
+			e.printStackTrace();
+			switch (e.getStatusCode()) {
+		    
+			case NOT_FOUND: throw new EmpleadosWebResourceNotFoundException();
+			
+			case FORBIDDEN: throw new EmpleadosWebNotAuthorizedException();
+			
+			default: throw new EmpleadosWebException();
+			}
+		}
+	}
+	
+	/* (non-Javadoc)
+	 * @see profe.empleados.web.service.EmpleadosWebService#getAllEmpleados()
+	 */
+	@Override
+	public List<Empleado> getAllEmpleados() {
+		Empleado[] Empleados = null;
+
+		try {
+			Empleados = restTemplate.getForObject(this.getBaseUrl()
+					+ "/empleados", Empleado[].class);
+		} catch (HttpClientErrorException e) {
+			logger.info("error en el getAllEmpleados");
+			e.printStackTrace();
+		}
+
+		if (Empleados == null || Empleados.length == 0)
+			return null;
+		else
+			return Arrays.asList(Empleados);
+	}
+
+	@Override
+	public void insertaEmpleado(Empleado empleado) 
+			throws EmpleadosWebNotAuthorizedException, EmpleadosWebResourceDuplicatedException {
+//		restTemplate.postForObject(this.getBaseUrl() + "/empleados", empleado, Empleado.class);
+		restTemplate.postForLocation(this.getBaseUrl() + "/empleados", empleado);
+	}
+
+	@Override
+	public void modificaEmpleado(Empleado empleado)
+			throws EmpleadosWebNotAuthorizedException, EmpleadosWebResourceNotFoundException {
+		restTemplate.put(this.getBaseUrl() + "/empleados/" + empleado.getCif(), empleado);
+	}
+
+}
